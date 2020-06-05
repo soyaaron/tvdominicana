a0be617f53eb69e97f74a2290800cf6c1a2818c4
Aaron Martinez <aaron@dix.do>
2020-06-04 21:28:32 -0400

    avance de json

--- lib/home2.dart
+++ lib/home2.dart
@@ -5,7 +5,7 @@ import 'package:tvdominicana/tvprofile.dart';
 class Homepage extends StatefulWidget {
   @override
   HomepageState createState() => new HomepageState();
-  List data;
+
 }
 
 class HomepageState extends State<Homepage> {
@@ -13,9 +13,10 @@ class HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text("TV Dominicana"),
+        title: Text("Televisión"),
       ),
       body: new Container(
+
         child: new Center(
           child: new FutureBuilder(
             future:
@@ -24,19 +25,22 @@ class HomepageState extends State<Homepage> {
               //Decode
               var mydata = json.decode(snapshot.data.toString());
               return new ListView.builder(
-
+                  
                 itemBuilder: (BuildContext context, int index) {
-
                     return ListTile(
-                      title:Text(
-                          "Name: " + mydata[index]["title"],
+                      leading:new Image.network(mydata[index]["imgURL"]), 
+                      title:
+                      Text(
+                          mydata[index]["title"],
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        subtitle: new Text(mydata[index]["subtitulo"]),
+                        subtitle: new Text("Canal " + mydata[index]["canal"]),
                         onTap: (){
-                          print("object");
-                          new MaterialPageRoute(builder: (context)=> TvProfile());
+                          print("presionado ");
+                          Navigator.push(context, 
+                            new MaterialPageRoute(builder: (context)=> TvProfile(),),
+                          );
                         },
                     );
 
@@ -48,12 +52,17 @@ class HomepageState extends State<Homepage> {
                   //       child: new Column(
                   //     crossAxisAlignment: CrossAxisAlignment.stretch,
                   //     children: <Widget>[
-                  //       new Text(
-                  //         "Name: " + mydata[index]["title"],
+                  //       Container( 
+                  //         width: 10,
+                  //         height: 70,
+                  //         child: new Image.network(mydata[index]["imgURL"]),
+                  //       ),
+                  //       //new Image.network(mydata[index]["imgURL"]),
+                  //       new Text(mydata[index]["title"],
                   //         style: TextStyle(
                   //             fontSize: 20, fontWeight: FontWeight.bold),
                   //       ),
-                  //       new Text(mydata[index]["subtitulo"]),
+                  //       new Text("Canal "+ mydata[index]["canal"]),
                   //     ],
                     
                   //   )
@@ -73,3 +82,11 @@ class HomepageState extends State<Homepage> {
   }
 }
 
+class TVinfo{
+  final String title;
+    final String canal;
+  final String imgURL;
+
+TVinfo(this.title, this.canal, this.imgURL);
+
+}
