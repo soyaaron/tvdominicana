f0822c9b517f1eb68cf59fb5a3c0d64a716d0c37
Aaron Martinez <aaronsenogara@gmail.com>
2020-06-08 10:39:28 -0400

    new json handling

--- lib/home2.dart
+++ lib/home2.dart
@@ -1,14 +1,30 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tvdominicana/tvprofile.dart';
+import 'package:tvdominicana/model.dart';
 
 class Homepage extends StatefulWidget {
   @override
   HomepageState createState() => new HomepageState();
-
 }
 
 class HomepageState extends State<Homepage> {
+  List data;
+
+  Future<String> loadJson() async {
+    var jsonText = await rootBundle.loadString('assets/test.json');
+
+    setState(() {
+      data = json.decode(jsonText);
+    });
+  }
+
+  @override
+  void initState() {
+    this.loadJson();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,63 +32,33 @@ class HomepageState extends State<Homepage> {
         title: Text("Televisión"),
       ),
       body: new Container(
-
         child: new Center(
           child: new FutureBuilder(
-            future:
-                DefaultAssetBundle.of(context).loadString('assets/test.json'),
             builder: (context, snapshot) {
-              //Decode
-              var mydata = json.decode(snapshot.data.toString());
               return new ListView.builder(
-                  
+                itemCount: data.length,
                 itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading:new Image.network(mydata[index]["imgURL"]), 
-                      title:
-                      Text(
-                          mydata[index]["title"],
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+                  return ListTile(
+                    leading: new Image.network(data[index]["imgURL"]),
+                    title: Text(
+                      data[index]["title"],
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: new Text("Canal " + data[index]["canal"]),
+                    onTap: () {
+                      print("presionado ");
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                          builder: (context) => TvProfile(),
                         ),
-                        subtitle: new Text("Canal " + mydata[index]["canal"]),
-                        onTap: (){
-                          print("presionado ");
-                          Navigator.push(context, 
-                            new MaterialPageRoute(builder: (context)=> TvProfile(),),
-                          );
-                        },
-                    );
-
-
-                  // return Padding(
-                  //   padding: const EdgeInsets.all(15),
-                  //   child: new Card(
-                      
-                  //       child: new Column(
-                  //     crossAxisAlignment: CrossAxisAlignment.stretch,
-                  //     children: <Widget>[
-                  //       Container( 
-                  //         width: 10,
-                  //         height: 70,
-                  //         child: new Image.network(mydata[index]["imgURL"]),
-                  //       ),
-                  //       //new Image.network(mydata[index]["imgURL"]),
-                  //       new Text(mydata[index]["title"],
-                  //         style: TextStyle(
-                  //             fontSize: 20, fontWeight: FontWeight.bold),
-                  //       ),
-                  //       new Text("Canal "+ mydata[index]["canal"]),
-                  //     ],
-                    
-                  //   )
-                    
-                  //   ),
-                  // );
+                      );
+                    },
+                  );
                 },
-                 
-                itemCount: mydata == null ? 0 : mydata.length,
-              
+
+                //  itemCount: data == null ? 0 : data.length,
               );
             },
           ),
@@ -82,11 +68,32 @@ class HomepageState extends State<Homepage> {
   }
 }
 
-class TVinfo{
-  final String title;
-    final String canal;
-  final String imgURL;
+//Decode
+//      var data = json.decode(snapshot.data.toString());
 
-TVinfo(this.title, this.canal, this.imgURL);
+//CARD CONCEPT
 
-}
+// return Padding(
+//   padding: const EdgeInsets.all(15),
+//   child: new Card(
+
+//       child: new Column(
+//     crossAxisAlignment: CrossAxisAlignment.stretch,
+//     children: <Widget>[
+//       Container(
+//         width: 10,
+//         height: 70,
+//         child: new Image.network(data[index]["imgURL"]),
+//       ),
+//       //new Image.network(data[index]["imgURL"]),
+//       new Text(data[index]["title"],
+//         style: TextStyle(
+//             fontSize: 20, fontWeight: FontWeight.bold),
+//       ),
+//       new Text("Canal "+ data[index]["canal"]),
+//     ],
+
+//   )
+
+//   ),
+// );
---
+++ lib/model.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+
+
+
+
+// // To parse this JSON data, do
+// //
+
+// final dataCanal = dataCanalFromJson('assets/test.json');
+
+// List<DataCanal> dataCanalFromJson(String str) =>
+//     List<DataCanal>.from(json.decode(str).map((x) => DataCanal.fromJson(x)));
+
+// String dataCanalToJson(List<DataCanal> data) =>
+//     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+// class DataCanal {
+//   DataCanal({
+//     this.canalid,
+//     this.title,
+//     this.canal,
+//     this.imgUrl,
+//     this.id,
+//   });
+
+//   int canalid;
+//   String title;
+//   String canal;
+//   String imgUrl;
+//   int id;
+
+//   factory DataCanal.fromJson(Map<String, dynamic> json) => DataCanal(
+//         canalid: json["canalid"],
+//         title: json["title"],
+//         canal: json["canal"],
+//         imgUrl: json["imgURL"],
+//         id: json["id"] == null ? null : json["id"],
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "canalid": canalid,
+//         "title": title,
+//         "canal": canal,
+//         "imgURL": imgUrl,
+//         "id": id == null ? null : id,
+//       };
+// }
+
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// final datacanal  =
+// class DataCanal {
+//   int canalid;
+//   String title;
+//   String subtitulo;
+//   String canal;
+//   String imgURL;
+
+//   DataCanal(
+//       {this.canalid, this.title, this.subtitulo, this.canal, this.imgURL});
+
+//   DataCanal.fromJson(Map<String, dynamic> json) {
+//     canalid = json['canalid'];
+//     title = json['title'];
+//     subtitulo = json['subtitulo'];
+//     canal = json['canal'];
+//     imgURL = json['imgURL'];
+//   }
+
+//   // Map<String, dynamic> toJson() {
+//   //   final Map<String, dynamic> data = new Map<String, dynamic>();
+//   //   data['canalid'] = this.canalid;
+//   //   data['title'] = this.title;
+//   //   data['subtitulo'] = this.subtitulo;
+//   //   data['canal'] = this.canal;
+//   //   data['imgURL'] = this.imgURL;
+//   //   return data;
+//   // }
+// }
